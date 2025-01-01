ad8cb66779aaedc74aa6f7e611570bd761de359b
AceCoder0 <hellozkr@163.com>
2025-01-01 22:33:56 +0800

    add bge

---
+++ parsers/html_similarity/lm_path.py
@@ -0,0 +1,3 @@
+lm_path = {
+    "BAAI/bge-large-zh-v1.5": "/mnt/ecf82360-d01d-4966-b234-c47ea01078db/hf_home/hub/models--BAAI--bge-large-zh-v1.5/snapshots/79e7739b6ab944e86d6171e44d24c997fc1e0116"
+}
